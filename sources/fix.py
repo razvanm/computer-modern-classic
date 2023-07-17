2f0d54aeea5a8aee04996131b870f95296a5ca65
Razvan Musaloiu-E <razvan@musaloiu.com>
2023-07-16 17:17:45 -0700

    Add 7 missing glyphs to Regular and Italic
    This commit adds the following 7 glyphs to Regular:
    
    - U+0022 QUOTATION MARK - copy of U+02DD (Double Acute Accent)
    - U+0027 APOSTROPHE - copy of U+00B4 (Acute Accent)
    - U+005C REVERSE SOLIDUS - copy of 0x6E from cmsy10
    - U+005E CIRCUMFLEX ACCENT - copy of U+02DC (Modifier Letter Circumflex Accent)
    - U+007E TILDE - copy of U+02DC (Small Tilde)
    - U+00A3 POUND SIGN - copy of the point from cmti10
    - U+00B0 DEGREE SIGN - copy of U+02DA (Ring Above)
    
    The same glyphs were added to Italic with one exception: instead of the
    U+00A3 POUND SIGN the U+0024 DOLLAR SIGN was added.
    
    This is the first step in addressing issue #1 for completing the GF
    Kernel character set.

--- sources/fix.py
+++ sources/fix.py
@@ -11,6 +11,11 @@ def copy_symbols(src, dest, glyph_names):
         dest.selection.select(g.glyphname)
         dest.paste()
 
+cmsy10 = fontforge.open('cmsy10.pfa')
+cmmi10 = fontforge.open('cmmi10.pfa')
+cmti10 = fontforge.open('cmti10.pfa')
+cmr10 = fontforge.open('cmr10.pfa')
+
 f = fontforge.open(sys.argv[1])
 
 space = f.createChar(0x20, 'space')
@@ -21,12 +26,44 @@ nbspace = f.createChar(0xA0, 'uni00A0')
 nbspace.width = 500
 nbspace.vwidth = 500
 
-if sys.argv[2].endswith('-Regular.ufo'):
-    copy_symbols(fontforge.open('cmsy10.pfa'), f, [
-        'bullet', 'minus', 'multiply', 'divide', 'periodcentered',
-        'paragraph', 'section', 'braceleft', 'braceright'])
+# Make the CIRCUMFLEX ACCENT (U+005E) a copy of "circumflex" (U+02C6,
+# 0x5E in cmr10).
+f['circumflex'].altuni = (0x005E,)
 
-    copy_symbols(fontforge.open('cmmi10.pfa'), f, [
-        'less', 'greater', 'slash'])
+# Make the TILDE (U+007E) a copy of "tilde" (U+02DC, 0x7E in cmr10).
+f['tilde'].altuni = (0x7E,)
+
+# Make the APOSTROPHE (U+0027) a copy of "acute" (U+00B4, 0x13 in cmr10).
+f['acute'].altuni = (0x0027,)
+
+# Make the QUOTATION MARK (U+0022) a copy of "hungarumlat" (U+02DD,
+# 0x7D in cmr10).
+f['hungarumlaut'].altuni = (0x0022,)
+
+# Make the DEGREE SIGN (U+00B0) a copy of "ring" (U+02DA, 0x17 in cmr10).
+f['ring'].altuni = (0x00B0,)
+
+copy_symbols(cmsy10, f, [
+    'bullet', 'minus', 'multiply', 'divide', 'periodcentered', 'paragraph',
+    'section', 'braceleft', 'braceright', 'backslash'])
+
+copy_symbols(cmmi10, f, ['less', 'greater', 'slash'])
+
+if sys.argv[2].endswith('-Regular.ufo'):
+    # Copy the POUND SIGN (U+00A3) from cmti10 where it shows up under the
+    # 'dollar' name (0x26 is the position in cmti10).
+    cmti10.selection.select('dollar')
+    cmti10.copy()
+    pound_sign = f.createChar(0x00A3, 'poundsign')
+    f.selection.select(pound_sign.glyphname)
+    f.paste()
+    cmti10.close()
+
+if sys.argv[2].endswith('-Italic.ufo'):
+    # The "dollar" is actually the pound sign.
+    f[0x0024].glyphname = 'poundsign'
+    f[0x0024].unicode = 0x00A3
+
+    copy_symbols(cmr10, f, ['dollar'])
 
 f.generate(sys.argv[2])
